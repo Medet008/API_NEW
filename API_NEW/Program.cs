aa602546557eb32cff73130ff4d7528f42468bb7
Медет Баймуратович <malyka.maryam@gmail.com>
2024-08-30 16:07:17 +0500

    Create SQL Tables and push Migrations

--- API_NEW/Program.cs
+++ API_NEW/Program.cs
@@ -5,12 +5,17 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDbConnection"));
+//});
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDbConnection"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultDbConnection"));
 });
 
-builder.Services.AddIdentity<IdentityUser,  IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser,  IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>(); 
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
