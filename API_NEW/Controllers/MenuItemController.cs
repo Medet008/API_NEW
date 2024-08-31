a084db5cc17db7411e8b2d0becd75176b2e8c094
Медет Баймуратович <malyka.maryam@gmail.com>
2024-08-31 23:08:25 +0500

    Update Menu Item

--- API_NEW/Controllers/MenuItemController.cs
+++ API_NEW/Controllers/MenuItemController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
 
+
 namespace API_NEW.Controllers
 {
     [Route("api/[controller]")]
@@ -96,6 +97,54 @@ namespace API_NEW.Controllers
                      = new List<string>() { ex.ToString() };
             }
 
+            return _response;
+        }
+
+
+        [HttpPut]
+        public async Task<ActionResult<ApiResponse>> UpdateMenuItem(int id, [FromForm] MenuItemUpdateDTO menuItemUpdateDTO)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if(menuItemUpdateDTO == null || id != menuItemUpdateDTO.Id)
+                    {
+                        return BadRequest(); 
+                    }
+
+                    MenuItem menuItemFromDb = await _db.MenuItems.FindAsync(id); 
+                    if(menuItemFromDb == null)
+                    {
+                        return BadRequest();    
+                    }
+                    menuItemFromDb.Name = menuItemUpdateDTO.Name;
+                    menuItemFromDb.Price = menuItemUpdateDTO.Price;
+                    menuItemFromDb.Category = menuItemUpdateDTO.Category;
+                    menuItemFromDb.SpecialTag = menuItemUpdateDTO.SpecialTag;
+                    menuItemFromDb.Description = menuItemUpdateDTO.Description; 
+
+                    if(menuItemUpdateDTO.File != null && menuItemUpdateDTO.File.Length >0)
+                    {
+                        string fileName = $"{Guid.NewGuid()}{Path.GetExtension(menuItemUpdateDTO.File.FileName)}";
+                        await _blobService.DeleteBlob(menuItemFromDb.Image.Split('/').Last(), SD.SD_Storage_Container);
+                        menuItemFromDb.Image = await _blobService.UploadBlob(fileName, SD.SD_Storage_Container, menuItemUpdateDTO.File);
+                    }
+
+                    _db.MenuItems.Update(menuItemFromDb);
+                    _db.SaveChanges();
+                    _response.StatusCode = HttpStatusCode.NoContent;
+                    return Ok(_response); 
+                }
+                else
+                {
+                    _response.IsSuccess = false; 
+                }
+            }catch (Exception ex)
+            {
+                _response.IsSuccess = false ;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
             return _response;
         }
     }
