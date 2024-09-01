6b7dbc96bdd3e3d0c21734a9fc739bae9802c018
Медет Баймуратович <malyka.maryam@gmail.com>
2024-09-01 17:10:22 +0500

    Add Authentication and Authorization Endpoints

---
+++ API_NEW/Controllers/AuthTestController.cs
@@ -0,0 +1,27 @@
+﻿using API_NEW.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_NEW.Controllers
+{
+    [Route("api/AuthTest")]
+    [ApiController]
+    public class AuthTestController : ControllerBase
+    {
+
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<string>> GetSomething()
+        {
+            return "You are authenticated"; 
+        }
+
+        [HttpGet("{id:int}")]
+        [Authorize(Roles = SD.Role_Admin)]
+        public async Task<ActionResult<string>> GetSomething(int SomeIntValue)
+        {
+            return "You are Authorized with Role of Admin"; 
+        }
+        
+    }
+}
