c886509383bf246bd2f93db94441f8cc43e4570d
Медет Баймуратович <malyka.maryam@gmail.com>
2024-09-07 20:02:37 +0500

    Get Shopping Cart

--- API_NEW/Controllers/ShoppingCartController.cs
+++ API_NEW/Controllers/ShoppingCartController.cs
@@ -3,24 +3,60 @@ using API_NEW.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 using System;
 using System.Net;
 
-namespace API_NEW.Controllers
+namespace RedMango_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ShoppingCartController : ControllerBase
     {
-
-        protected ApiResponse _response;  
+        protected ApiResponse _response;
         private readonly ApplicationDbContext _db;
-        public ShoppingCartController(ApplicationDbContext db) {
+        public ShoppingCartController(ApplicationDbContext db)
+        {
+            _response = new();
             _db = db;
-            _response = new ();
         }
 
         [HttpGet]
+        public async Task<ActionResult<ApiResponse>> GetShoppingCart(string userId)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(userId))
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
+                ShoppingCart shoppingCart = _db.ShoppingCarts
+                    .Include(u => u.CartItems).ThenInclude(u => u.MenuItem)
+                    .FirstOrDefault(u => u.UserId == userId);
+
+                if (shoppingCart.CartItems != null && shoppingCart.CartItems.Count > 0)
+                {
+                    shoppingCart.CartTotal = shoppingCart.CartItems.Sum(u => u.Quantity * u.MenuItem.Price);
+                }
+                _response.Result = shoppingCart;
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+                _response.StatusCode = HttpStatusCode.BadRequest;
+            }
+            return _response;
+        }
+
+
+
+        [HttpPost]
         public async Task<ActionResult<ApiResponse>> AddOrUpdateItemInCart(string userId, int menuItemId, int updateQuantityBy)
         {
 
@@ -36,39 +72,91 @@ namespace API_NEW.Controllers
             // когда пользователь обновляет количество существующего товара
             // когда пользователь удаляет существующий товар
 
-            ShoppingCart shoppingCart = _db.ShoppingCarts.Include(u =>u.CartItems).FirstOrDefault(u => u.UserId == userId);
-            MenuItem menuItem = _db.MenuItems.FirstOrDefault(u => u.Id == menuItemId);  
-
+            ShoppingCart shoppingCart = _db.ShoppingCarts.Include(u => u.CartItems).FirstOrDefault(u => u.UserId == userId);
+            MenuItem menuItem = _db.MenuItems.FirstOrDefault(u => u.Id == menuItemId);
             if (menuItem == null)
             {
                 _response.StatusCode = HttpStatusCode.BadRequest;
                 _response.IsSuccess = false;
-                return BadRequest(_response); 
+                return BadRequest(_response);
             }
-            if(shoppingCart == null && updateQuantityBy > 0) 
+            if (shoppingCart == null && updateQuantityBy > 0)
             {
-                ShoppingCart newCart = new() { UserId = userId }; 
+                //create a shopping cart & add cart item
+                ShoppingCart newCart = new() { UserId = userId };
                 _db.ShoppingCarts.Add(newCart);
                 _db.SaveChanges();
-
                 CartItem newCartItem = new()
                 {
                     MenuItemId = menuItemId,
                     Quantity = updateQuantityBy,
-                    ShoppingCartId = shoppingCart.Id, 
-                    MenuItem=null
-                }; 
-
+                    ShoppingCartId = newCart.Id,
+                    MenuItem = null
+                };
                 _db.CartItems.Add(newCartItem);
                 _db.SaveChanges();
             }
             else
             {
-                
+                //shopping cart exists
+                // корзина покупок существует
+
+                //CartItem cartIteminDb = _db.CartItems.FirstOrDefault(u => u.MenuItemId == menuItemId);
+                CartItem cartItemInCart = shoppingCart.CartItems.FirstOrDefault(u => u.MenuItemId == menuItemId);
+                if (cartItemInCart == null)
+                {
+                    //item does not exist in current cart
+                    // товар отсутствует в текущей корзине
+                    CartItem newCartItem = new()
+                    {
+                        MenuItemId = menuItemId,
+                        Quantity = updateQuantityBy,
+                        ShoppingCartId = shoppingCart.Id,
+                        MenuItem = null
+                    };
+                    _db.CartItems.Add(newCartItem);
+                    _db.SaveChanges();
+                }
+                else
+                {
+                    //item already exist in the cart and we have to update quantity
+                    // товар уже существует в корзине, необходимо обновить количество
+                    int newQuantity = cartItemInCart.Quantity + updateQuantityBy;
+                    if (updateQuantityBy == 0 || newQuantity <= 0)
+                    {
+                        //remove cart item from cart and if it is the only item then remove cart
+                        // удалить товар из корзины, и если это единственный товар, удалить корзину
+                        _db.CartItems.Remove(cartItemInCart);
+                        if (shoppingCart.CartItems.Count() == 1)
+                        {
+                            _db.ShoppingCarts.Remove(shoppingCart);
+                        }
+                        _db.SaveChanges();
+                    }
+                    else
+                    {
+                        cartItemInCart.Quantity = newQuantity;
+                        _db.SaveChanges();
+                    }
+                }
             }
             return _response;
+
         }
-
-
     }
 }
+
+
+
+
+// Корзина покупок будет иметь одну запись на каждый идентификатор пользователя, даже если у пользователя много товаров в корзине.
+// Cart items будут содержать все товары в корзине покупок для пользователя.
+// updatequantityby будет содержать количество, на которое нужно обновить количество товара.
+// Если это -1, это означает, что мы уменьшаем количество; если это 5, это означает, что нужно добавить 5 к существующему количеству.
+// Если updatequantityby равен 0, товар будет удален.
+
+
+// когда пользователь добавляет новый товар в новую корзину покупок в первый раз
+// когда пользователь добавляет новый товар в уже существующую корзину покупок (у пользователя уже есть другие товары в корзине)
+// когда пользователь обновляет количество существующего товара
+// когда пользователь удаляет существующий товар
