ef90655d958130ae5e19021690e4beeaacaa2060
Медет Баймуратович <malyka.maryam@gmail.com>
2024-09-08 15:52:47 +0500

    Order Header and Details Model

--- API_NEW/Data/ApplicationDbContext.cs
+++ API_NEW/Data/ApplicationDbContext.cs
@@ -16,9 +16,10 @@ namespace API_NEW.Data
 
         public DbSet<ApplicationUser> ApplicationUsers  { get; set; }
         public DbSet<MenuItem> MenuItems { get; set; }
-
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
-        public DbSet<CartItem> CartItems { get; set; } 
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; } 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
