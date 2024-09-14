2a5cd28054537332fc21fe0bc3bf19810f1a4a3c
Медет Баймуратович <malyka.maryam@gmail.com>
2024-09-14 22:22:34 +0500

    Stripe Payment Endpoint

---
+++ API_NEW/Controllers/PaymentController.cs
@@ -0,0 +1,48 @@
+﻿using API_NEW.Data;
+using API_NEW.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace RedMango_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        protected ApiResponse _response;
+        private readonly IConfiguration _congifuration;
+        private readonly ApplicationDbContext _db;
+        public PaymentController(IConfiguration configuration, ApplicationDbContext db)
+        {
+            _congifuration = configuration;
+            _db = db;
+            _response = new();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> MakePayment(string userId)
+        {
+            ShoppingCart shoppingCart = _db.ShoppingCarts
+                .Include(u => u.CartItems)
+                .ThenInclude(u => u.MenuItem).FirstOrDefault(u => u.UserId == userId);
+
+            if (shoppingCart == null || shoppingCart.CartItems == null || shoppingCart.CartItems.Count() == 0)
+            {
+                _response.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest(_response);
+            }
+
+            #region Create Payment Intent
+
+
+            #endregion
+
+            _response.Result = shoppingCart;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+
+    }
+}
